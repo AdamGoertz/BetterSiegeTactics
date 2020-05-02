75bdf60f75d2704a35c8145714bc8ed369e15a08
AdamGoertz <adambgoertz@gmail.com>
2020-05-02 01:29:46 -0500

    fixed buggy implementation of isSupported

--- src/main/java/den0minat0r/bettersiegetactics/common/block/SupportedBlock.java
+++ src/main/java/den0minat0r/bettersiegetactics/common/block/SupportedBlock.java
@@ -1,28 +1,23 @@
 package den0minat0r.bettersiegetactics.common.block;
 
-import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayDeque;
-import java.util.HashSet;
-
-import javax.annotation.Nullable;
-
-import org.apache.logging.log4j.LogManager;
+import java.util.HashMap;
+import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.ItemStack;
+import net.minecraft.block.FallingBlock;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class SupportedBlock extends Block {
+public class SupportedBlock extends FallingBlock {
 	
-	private static final Logger LOGGER = LogManager.getLogger(); 
     private static final Direction[] SUPPORT_SEARCH_ORDER = new Direction[]{Direction.WEST, Direction.EAST, Direction.NORTH, Direction.SOUTH};
-    private static final int MAX_SEARCH_DISTANCE = 32;
 	private final int support_distance;
 
 	public SupportedBlock(Properties properties, int support_distance) {
@@ -31,28 +26,17 @@ public class SupportedBlock extends Block {
 		this.support_distance = support_distance;
 		
 	}
-
-	@Override
-	public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
-		if (!isSupported(worldIn, pos)) {
-			fall(state, worldIn, pos);
-		}
-	}
 	
 	@Override
-	public void neighborChanged(BlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos, boolean isMoving) {
-		BlockState fromState = worldIn.getBlockState(fromPos);
-		if (!fromState.isSolid()) // TODO: What if the neighbor is changed to a block with lower support_distance? 
+	public void func_225534_a_(BlockState state, ServerWorld sWorld, BlockPos pos, Random rand) {
+		if (!isSupported(sWorld.getWorld(), pos)) 
 		{
-			if (!this.isSupported(worldIn, pos)) 
-			{
-				fall(state, worldIn, pos);
-			}
+			super.func_225534_a_(state, sWorld, pos, rand);
 		}
 	}
 	
 	public static boolean isDirectlySupported(World worldIn, BlockPos pos) {
-		return worldIn.getBlockState(pos.down()).isSolid();
+		return pos.getY() == 0 || !FallingBlock.canFallThrough(worldIn.getBlockState(pos.down()));
 	}
 	
 	public int getSupportDistance() {
@@ -61,49 +45,33 @@ public class SupportedBlock extends Block {
 	
 	public boolean isSupported(World worldIn, BlockPos pos) {
 		ArrayDeque<BlockPos> search_queue = new ArrayDeque<>(4*this.support_distance);
-		HashSet<BlockPos> discovered_blocks = new HashSet<>(4*this.support_distance);
+		HashMap<BlockPos, Integer> discovered_blocks = new HashMap<>(4*this.support_distance);
+
 		search_queue.addLast(pos);
-		return isSupportedHelper(worldIn, 0, search_queue, discovered_blocks);
-	}
-	
-	private static boolean isSupportedHelper(World worldIn, int distance, ArrayDeque<BlockPos> search_queue, HashSet<BlockPos> discovered_blocks) {
-		BlockPos current_pos = search_queue.pollFirst();
+		discovered_blocks.put(pos, 0);
 
-		if (current_pos == null)
+		BlockPos current_pos;
+		while ((current_pos = search_queue.pollFirst()) != null)
 		{
-			return false; 
-		}
+			int current_distance = discovered_blocks.get(current_pos);
 
-		discovered_blocks.add(current_pos);
-
-		if (isDirectlySupported(worldIn, current_pos))
-		{
-			return true;
-		}
-		else
-		{
-			Block current_block = worldIn.getBlockState(current_pos).getBlock();
-			
-			if (current_block instanceof SupportedBlock && distance >= ((SupportedBlock)current_block).getSupportDistance())
+			if (isDirectlySupported(worldIn, current_pos))
 			{
-				return false;
-			}
-			else if (distance >= SupportedBlock.MAX_SEARCH_DISTANCE)
-			{
-				return false;
+				return true;
 			}
 			
 			for (Direction dir: SupportedBlock.SUPPORT_SEARCH_ORDER)
 			{
 				BlockPos next_pos = current_pos.offset(dir);
-				if (!discovered_blocks.contains(next_pos) && worldIn.getBlockState(next_pos).isSolid())
+				if (!discovered_blocks.containsKey(next_pos) && worldIn.getBlockState(next_pos).isSolid() && current_distance < this.support_distance)
 				{
+					discovered_blocks.put(next_pos, current_distance + 1);
 					search_queue.addLast(next_pos);
 				}
 			}
-			
-			return isSupportedHelper(worldIn, distance + 1, search_queue, discovered_blocks);
 		}
+
+		return false;
 	}
 	
 	/**
@@ -113,18 +81,18 @@ public class SupportedBlock extends Block {
 	 * @param pos the position of the source {@link Block}.
 	 * @return the {@link BlockPos} of the lowest air {@link Block} above a solid block .
 	 */
-	public BlockPos getPosAboveNearestSolidBlock(World worldIn, BlockPos pos) {
-		while (!worldIn.getBlockState(pos.down()).isSolid()) {
+	public static BlockPos getPosAboveNearestSolidBlock(World worldIn, BlockPos pos) {
+		while (pos.getY() >= 0 && FallingBlock.canFallThrough(worldIn.getBlockState(pos.down()))) {
 			pos = pos.down();
 		}
 		return pos;
 	}
 	
-	public void fall(BlockState state, World worldIn, BlockPos pos) {
-		BlockPos fall_pos = getPosAboveNearestSolidBlock(worldIn, pos);
-		BlockState ground_state = worldIn.getBlockState(fall_pos);
-		Block.replaceBlock(ground_state, state, worldIn, fall_pos, 0);
-		worldIn.setBlockState(pos, Blocks.AIR.getDefaultState());
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+    public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
+		// Do not spawn particles.
 	}
 	
 }
