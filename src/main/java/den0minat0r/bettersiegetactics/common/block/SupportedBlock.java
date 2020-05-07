a535b66780b5d9f5d785f88aaef2244f926bdab1
AdamGoertz <adambgoertz@gmail.com>
2020-05-07 00:14:26 -0500

    removed cache, SupportedBlock updating working in SP, not sure if compatible with multiplayer (probably not).

--- src/main/java/den0minat0r/bettersiegetactics/common/block/SupportedBlock.java
+++ src/main/java/den0minat0r/bettersiegetactics/common/block/SupportedBlock.java
@@ -9,9 +9,6 @@ import java.util.Set;
 import javax.annotation.Nullable;
 
 import org.apache.logging.log4j.Logger;
-
-import den0minat0r.bettersiegetactics.common.util.SupportedBlockCache;
-
 import org.apache.logging.log4j.LogManager;
 
 import net.minecraft.block.BlockState;
@@ -31,10 +28,10 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class SupportedBlock extends FallingBlock {
 	
 	private static final Logger LOGGER = LogManager.getLogger();
-    private static final Direction[] SUPPORT_SEARCH_ORDER = new Direction[] {Direction.WEST, Direction.EAST, Direction.NORTH, Direction.SOUTH};
-    private static final Direction[] SUPPORT_UPDATE_ORDER = new Direction[] {Direction.WEST, Direction.EAST, Direction.NORTH, Direction.SOUTH, Direction.UP};
-	private static final SupportedBlockCache cache = new SupportedBlockCache(1024);
+	private static final Direction[] SUPPORT_SEARCH_ORDER = new Direction[] { Direction.WEST, Direction.EAST, Direction.NORTH, Direction.SOUTH };
+	private static final Direction[] SUPPORT_UPDATE_ORDER = new Direction[] { Direction.WEST, Direction.EAST, Direction.NORTH, Direction.SOUTH, Direction.UP };
 	private final int support_distance;
+	private ServerWorld server;
 
 	public SupportedBlock(Properties properties, int support_distance) {
 		super(properties);
@@ -44,27 +41,27 @@ public class SupportedBlock extends FallingBlock {
 	
 	@Override 
     public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack stack) {
-		if (!isSupported(worldIn, pos)) {
+		LOGGER.debug("Placed block "+pos.getX()+","+pos.getY()+","+pos.getZ());
+		if (worldIn.isAreaLoaded(pos, 32) && !isSupported(worldIn, pos)) {
 			fall(worldIn, pos);
 		}
 	}
 
 	@Override
-	public void func_225534_a_(BlockState state, ServerWorld sWorld, BlockPos pos, Random rand) {
-		// Do nothing. This functionality is handled elsewhere.
+	public void func_225534_a_(BlockState state, ServerWorld sWorld, BlockPos pos, @Nullable Random rand) {
+		LOGGER.debug("Tick Executed");
+		this.server = sWorld;
+		if (sWorld.isAreaLoaded(pos,  32) && !isSupported(sWorld, pos)) {
+			fall(sWorld, pos);
+		}
 	}
 	
 	@Override 
-	public void onStartFalling(FallingBlockEntity entity) {
+	protected void onStartFalling(FallingBlockEntity entity) {
 		LOGGER.debug("Falling...");
 		entity.setHurtEntities(true);
 	}
 	
-	@Override 
-	public void onEndFalling(World worldIn, BlockPos pos, BlockState fallingState, BlockState hitState) {
-		LOGGER.debug("Landed.");
-		cache.put(pos.down(), pos);
-	}
 	
 	public static boolean isDirectlySupported(World worldIn, BlockPos pos) {
 		return pos.getY() == 0 || !FallingBlock.canFallThrough(worldIn.getBlockState(pos.down()));
@@ -76,58 +73,59 @@ public class SupportedBlock extends FallingBlock {
 	
 	@Override
 	public void onPlayerDestroy(IWorld worldIn, BlockPos pos, BlockState state) {
-		LOGGER.debug("Destroyed by player.");
 		updateAfterDestroy(worldIn, pos);
 	}
 	
 	@Override
 	public void onExplosionDestroy(World worldIn, BlockPos pos, Explosion explosionIn) {
-		LOGGER.debug("Destroyed by explosion.");
 		updateAfterDestroy(worldIn, pos);
 	}
 	
-
 	private void updateAfterDestroy(IWorld worldIn, BlockPos pos) {
-		cache.remove(pos);
-		
-		for (BlockPos checkPos : getAllBlocksInSupportRange(worldIn.getWorld(), pos, SupportedBlock.SUPPORT_UPDATE_ORDER)) {
-			if (!isSupported(worldIn.getWorld(), checkPos)) {
-				fall(worldIn, checkPos);
+		if (worldIn.isAreaLoaded(pos, 32)) {
+			Set<BlockPos> blocks = getAllConnectedBlocksInRange(worldIn.getWorld(), pos, SUPPORT_UPDATE_ORDER, this.support_distance + 1);
+			LOGGER.debug("Updating "+blocks.size()+" blocks.");
+			for (BlockPos checkPos : blocks) {
+				if (this.server != null) {
+					this.server.getPendingBlockTicks().scheduleTick(checkPos, this, 0);
+				}
 			}
 		}
 	}
-	
+
 	private void fall(IWorld worldIn, BlockPos pos) {
-		FallingBlockEntity fallingblockentity = new FallingBlockEntity(worldIn.getWorld(), (double)pos.getX() + 0.5D, (double)pos.getY(), (double)pos.getZ() + 0.5D, worldIn.getBlockState(pos));
-        this.onStartFalling(fallingblockentity);
-        worldIn.addEntity(fallingblockentity);
-        cache.remove(pos);
+		if (this.server != null)
+		{
+			FallingBlockEntity fallingblockentity = new FallingBlockEntity(this.server, (double)pos.getX() + 0.5D, (double)pos.getY(), (double)pos.getZ() + 0.5D, worldIn.getBlockState(pos));
+			this.onStartFalling(fallingblockentity);
+			this.server.addEntity(fallingblockentity);
+		}
 	}
 	
 	/**
-	 * Perform a breadth-first search for any block within Manhattan-distance {@value this.support_distance} that rests on top of a solid block.
 	 * @param worldIn the {@link World}. 
 	 * @param pos the block's {@link BlockPos}.
 	 * @return {@code true} if the block is supported else {@code false}.
 	 */
 	public boolean isSupported(World worldIn, BlockPos pos) {
-		if (cache.isSupported(pos)) {
-			return true;
+		if (worldIn.isAreaLoaded(pos, 32)) {
+			LOGGER.debug("Checking for support at "+pos.getX()+","+pos.getY()+","+pos.getZ());
+			Set<BlockPos> blocks = getAllConnectedBlocksInRange(worldIn, pos, SUPPORT_SEARCH_ORDER, this.support_distance);
+			LOGGER.debug("Checking "+blocks.size()+" blocks.");
+			for (BlockPos checkPos : blocks) {
+				if (SupportedBlock.isDirectlySupported(worldIn, checkPos)) {
+					return true;
+				}
+			}
+			return false;
 		}
 		
-		for (BlockPos checkPos : getAllBlocksInSupportRange(worldIn, pos, SupportedBlock.SUPPORT_SEARCH_ORDER)) {
-			if (SupportedBlock.isDirectlySupported(worldIn, checkPos)) {
-				cache.put(checkPos, pos);
-				return true;
-			}
-		}
-	
-		return false;
+		return true;
 	}
 	
-	private Set<BlockPos> getAllBlocksInSupportRange(World worldIn, BlockPos pos, Direction[] searchDirections) {
-		ArrayDeque<BlockPos> search_queue = new ArrayDeque<>(4*this.support_distance);
-		LinkedHashMap<BlockPos, Integer> discovered_blocks = new LinkedHashMap<>((2*this.support_distance*this.support_distance) + (2*this.support_distance) + 1);
+	private static Set<BlockPos> getAllConnectedBlocksInRange(World worldIn, BlockPos pos, Direction[] searchDirections, int range) {
+		ArrayDeque<BlockPos> search_queue = new ArrayDeque<>(4*range);
+		LinkedHashMap<BlockPos, Integer> discovered_blocks = new LinkedHashMap<>((2*range*range) + (2*range) + 1);
 
 		search_queue.addLast(pos);
 		discovered_blocks.put(pos, 0);
@@ -141,18 +139,22 @@ public class SupportedBlock extends FallingBlock {
 			{
 				BlockPos next_pos = current_pos.offset(dir);
 
-				if (!discovered_blocks.containsKey(next_pos) && worldIn.getBlockState(next_pos).isSolid() && current_distance < this.support_distance)
+				if (!discovered_blocks.containsKey(next_pos) && worldIn.getBlockState(next_pos).isSolid() && current_distance < range)
 				{
 					discovered_blocks.put(next_pos, current_distance + 1);
 					search_queue.addLast(next_pos);
 				}
 			}
 		}
+
+		// Remove initial block if it is not solid... was needed previously to provide root of tree.
+		if (!worldIn.getBlockState(pos).isSolid()) {
+			discovered_blocks.remove(pos);
+		}
 		
-		return discovered_blocks.keySet();
+		return discovered_blocks.keySet();	
 	}
 	
-
 	@Override
 	@OnlyIn(Dist.CLIENT)
     public void animateTick(BlockState stateIn, World worldIn, BlockPos pos, Random rand) {
